ee7902d34c0d959ee649ff9cc4b0422543f3fe01
Maggie Goodwin <magoodwin@wpi.edu>
2020-01-27 22:06:09 -0500

    committing changes for safety. currently refactoring build options

--- BrainEx/src/Components/BuildOptions.js
+++ BrainEx/src/Components/BuildOptions.js
@@ -9,15 +9,9 @@ import logo from "../brain.png";
 // todo refactor and make more dynamic, also use full page
 const buildOptions = () => (
         <Container className="container" style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-start', width: '100%' }}>
-            <div style={{backgroundColor: '#3C99DC', width: '100%', height: '17%', position: 'absolute', left:'0.01%', top:'0%'}}>
-                <img src={logo} className="App-logo" alt="logo"/>
-                <h1 className="App-title">BrainEx</h1>
-                <h3 className="App-Version">Version 1.0.0</h3>
-                <h4 className="options">Preprocessing Options</h4>
-            </div>
 
-            <label htmlFor="dropdown" style={{position:'absolute', left:'45%', top:'22%'}}>Distance Type</label>
-            <div className="dropdown" style={{position:'absolute', left:'41.5%', top:'27%'}}>
+            <label htmlFor="dropdown">Distance Type</label>
+            <div className="dropdown">
                 <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
                         data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Choose a Distance
@@ -30,35 +24,35 @@ const buildOptions = () => (
                 </div>
             </div>
 
-            <Button className="distHelp" style={{position:'absolute', left:'75%', top:'28%', borderColor: 'black', backgroundColor:'#0F5298'}}>?</Button>
+            <Button className="distHelp">?</Button>
 
             <form>
-                <div className="form-group" style={{width: '40%', position:'absolute', left:'30%', top:'42%'}}>
+                <div className="form-group">
                     <label htmlFor="formControlRange">Similarity Threshold</label>
                     <input type="range" className="form-control-range" id="formControlRange"/>
                 </div>
             </form>
 
-            <Button className="simHelp" style={{position:'absolute', left:'75%', top:'43%', borderColor: 'black', backgroundColor:'#0F5298'}}>?</Button>
+            <Button className="simHelp">?</Button>
 
             <form>
-                <div className="form-group" style={{width: '40%', position:'absolute', left:'30%', top:'60%'}}>
+                <div className="form-group">
                     <label htmlFor="formControlRange">Length of Interest</label>
                     <input type="range" className="form-control-range" id="formControlRange"/>
                 </div>
             </form>
 
-            <Button className="lenHelp" style={{position:'absolute', left:'75%', top:'61%', borderColor: 'black', backgroundColor:'#0F5298'}}>?</Button>
+            <Button className="lenHelp">?</Button>
 
 
             <Link to="/BuildProgressMenu">
-                <Button className="startprocess btn-primary" style={{width: '20%', borderColor: 'black', backgroundColor:'#0F5298'}}>Start Preprocessing</Button>
+                <Button className="startprocess btn-primary">Start Preprocessing</Button>
             </Link>
 
             <Container className="Box"> </Container>
 
             <Link to="/SelectNewDataset">
-                <Button className="back btn-primary" style={{width: '12%', borderColor: 'black', backgroundColor:'#0F5298'}}>Back</Button>
+                <Button className="back btn-primary">Back</Button>
             </Link>
 
         </Container>
--- BrainEx/src/Components/SelectNewDataset.js
+++ BrainEx/src/Components/SelectNewDataset.js
@@ -58,33 +58,3 @@ class SelectNewDataset extends Component {
 }
 
 export default SelectNewDataset;
-
-/*<div className="col-sm-4">
-                    <div className="well">
-                        <Card>
-                            <div className="card-body">
-                                <h5 className="card-title">Previously imported unpreprocessed CSV Files</h5>
-                                <div className="scrollable" style={{overflowY: 'auto', maxHeight: '210px'}}>
-                                    <Button style={{ borderColor: 'black', width: '100%', backgroundColor:'#0F5298'}}>Dataset1</Button>
-                                    <p> </p>
-                                    <Button style={{ borderColor: 'black', width: '100%', backgroundColor:'#0F5298'}}>Dataset2</Button>
-                                    <p> </p>
-                                    <Button style={{ borderColor: 'black', width: '100%', backgroundColor:'#0F5298'}}>Dataset3</Button>
-                                    <p> </p>
-                                    <Button style={{ borderColor: 'black', width: '100%', backgroundColor:'#0F5298'}}>Dataset4</Button>
-                                    <p> </p>
-                                    <Button style={{ borderColor: 'black', width: '100%', backgroundColor:'#0F5298'}}>Dataset5</Button>
-                                    <p> </p>
-                                    <Button style={{ borderColor: 'black', width: '100%', backgroundColor:'#0F5298'}}>Dataset6</Button>
-                                </div>
-                            </div>
-                        </Card>
-                    </div>
-                </div>
-                <Link to="/">
-                    <Button className="back btn-primary" style={{width: '10%', position: 'absolute', top: '90%', left: '33%', backgroundColor:'#0F5298', borderColor: 'black'}}>Back</Button>
-                </Link>
-                <Link to="/BuildOptions">
-                    <Button className="nextbtn btn-primary" style={{width: '10%', position: 'absolute', top: '90%', left: '88%', backgroundColor:'#0F5298', borderColor: 'black'}}>Next</Button>
-                </Link>
-            </div>*/
