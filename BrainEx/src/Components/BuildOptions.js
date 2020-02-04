5f451ae3b8c6a93f3fe49355213fa17ace3b49ab
mags1 <magoodwin@wpi.edu>
2020-02-04 18:22:58 -0500

    refactored form to be table based

--- BrainEx/src/Components/BuildOptions.js
+++ BrainEx/src/Components/BuildOptions.js
@@ -14,12 +14,14 @@ class BuildOptions extends Component {
             feature_val: 5,
             distance_val: "eu",
             sim_val: 0, /*[0:1]*/ /*todo get the desired default value*/
-            loi_val: 100 /*[0:max length]*/ /*todo change this to pull in the length of the longest time series*/
+            loi_val: 100, /*[0:max length]*/ /*todo change this to pull in the length of the longest time series*/
+            spark_val: true /*todo should the default be yes/true?*/
         };
         this.update_feature = this.update_feature.bind(this);
         this.update_distance = this.update_distance.bind(this);
         this.update_sim = this.update_sim.bind(this);
         this.update_loi = this.update_loi.bind(this);
+        this.update_spark = this.update_spark.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
@@ -57,6 +59,13 @@ class BuildOptions extends Component {
         });
     };
 
+    update_spark = (e) => {
+        const spark_val = e.target.checked;
+        this.setState({
+            spark_val: spark_val
+        });
+    };
+
     // submit the content to the parent component (App.js) and proceed to the next page
     handleSubmit = (e) => {
         e.preventDefault();
@@ -69,72 +78,108 @@ class BuildOptions extends Component {
     render() {
         return(
             <div className="full-height">
-                <form className="row no-gutters" onSubmit={this.handleSubmit}>
-                    <div className="col no-gutters d-flex justify-content-center">{/*general build options*/}
-                        <div className="build_form left-side">
-                            {/*form input 1*/}
-                            <div className="form-group d-flex justify-content-sm-around">
-                                <label className="form_label" htmlFor="feature_num">Feature Number:</label>
-                                <input className="form-control" type="number" value={this.state.feature_val} id="feature_num" onChange={this.update_feature}/>
-                            </div>
-                            {/*form input 2*/}
-                            <div className="form-group d-flex justify-content-sm-around">
-                                <label className="form_label" htmlFor="distance_type">Distance Type:</label>
-                                <select className="form-control" id="distance_type" value={this.state.distance_val} onChange={this.update_distance}>
-                                    <option value="eu">Warped Euclidean</option>
-                                    <option value="ma">Warped Manhattan</option>
-                                    <option value="mi">Warped Minkowski</option>
-                                    <option value="ch">Warped Chebyshev</option>
-                                </select>
-                            </div>
-                            {/*form input 3*/}
-                            <div className="form-group range-field d-flex justify-content-sm-around">
-                                <label className="form_label" htmlFor="sim_thresh">Similarity Threshold:</label>
-                                <span className="font-weight-bold indigo-text">0%</span>
-                                {/*todo update "defaultValue" to be recommended similarity threshold */}
-                                <input type="range" className="custom-range" id="sim_thresh" min="0" max="100" value={this.state.sim_val} onChange={this.update_sim}/>
-                                <span className="font-weight-bold indigo-text">{this.state.sim_val}%</span>
-                            </div>
-                            {/*form input 4*/}
-                            <div className="form-group range-field d-flex justify-content-sm-around">
-                                {/*todo range "max" will have to be dynamically set by passing props from
+                <form className="build_form" onSubmit={this.handleSubmit}>
+                    {/*form input 1*/}
+                    <tr className="form-group">
+                        <td className="form_label">
+                            <label className="fl" htmlFor="feature_num">Feature Number:</label>
+                        </td>
+                        <td className="form_input">
+                            <input
+                                className="form-control"
+                                type="number"
+                                value={this.state.feature_val}
+                                id="feature_num"
+                                onChange={this.update_feature}/>
+                        </td>
+                    </tr>
+                    {/*form input 2*/}
+                    <tr className="form-group">
+                        <td className="form_label">
+                            <label className="fl" htmlFor="distance_type">Distance Type:</label>
+                        </td>
+                        <td className="form_input">
+                            <select
+                                className="form-control"
+                                id="distance_type"
+                                value={this.state.distance_val}
+                                onChange={this.update_distance}>
+                                <option value="eu">Warped Euclidean</option>
+                                <option value="ma">Warped Manhattan</option>
+                                <option value="mi">Warped Minkowski</option>
+                                <option value="ch">Warped Chebyshev</option>
+                            </select>
+                        </td>
+                    </tr>
+                    {/*form input 3*/}
+                    <tr className="form-group">
+                        <td className="form_label">
+                            <label className="fl" htmlFor="sim_thresh">Similarity Threshold:</label>
+                        </td>
+                        <td className="form_input is_range">
+                            <span className="font-weight-bold indigo-text">0%</span>
+                            {/*todo update "defaultValue" to be recommended similarity threshold */}
+                            <input
+                                type="range"
+                                className="custom-range"
+                                id="sim_thresh"
+                                min="0" max="100"
+                                value={this.state.sim_val}
+                                onChange={this.update_sim}/>
+                            <span className="font-weight-bold indigo-text">{this.state.sim_val}%</span>
+                        </td>
+                    </tr>
+                    {/*form input 4*/}
+                    <tr className="form-group">
+                        {/*todo range "max" will have to be dynamically set by passing props from
                                     SelectADataset into BuildOptions state */}
-                                {/*todo update "defaultValue" to be same as max */}
-                                <label className="form_label" htmlFor="loi">Length of Interest:</label>
-                                <span className="font-weight-bold indigo-text">0</span>
-                                <input type="range" className="custom-range" id="loi" min="0" max="100" value={this.state.loi_val} onChange={this.update_loi}/>
-                                <span className="font-weight-bold indigo-text">{this.state.loi_val}</span>
+                        {/*todo update "defaultValue" to be same as max */}
+                        <td className="form_label">
+                            <label className="fl" htmlFor="loi">Length of Interest:</label>
+                        </td>
+                        <td className="form_input is_range">
+                            <span className="font-weight-bold indigo-text">0</span>
+                            <input
+                                type="range"
+                                className="custom-range"
+                                id="loi"
+                                min="0" max="100"
+                                value={this.state.loi_val}
+                                onChange={this.update_loi}/>
+                            <span className="font-weight-bold indigo-text">{this.state.loi_val}</span>
+                        </td>
+                    </tr>
+                    {/*form input 5*/}
+                    <tr className="form-group">
+                        <td className="form_label">
+                            <label className="fl" htmlFor="use_spark">Use Spark:</label>
+                        </td>
+                        {/*todo should the default be spark or no spark*/}
+                        <td className="form_input">
+                            <div className="form-check">
+                                <input
+                                    className="form-control"
+                                    type="checkbox"
+                                    id="use_spark"
+                                    checked={this.state.spark_val}
+                                    onChange={this.update_spark}/>
                             </div>
-                            {/*form input 5*/}
-                            <div className="form-group range-field d-flex justify-content-sm-around">
-                                <div className="form-check">
-                                    <label className="form-check-label" htmlFor="use-spark">Use Spark</label>
-                                    <input className="form-check-input" type="checkbox" value="" id="use-spark"/>
-                                </div>
-                                <button type="button" id="formButton">Toggle Form!</button>
-                                {/*
-                                <li><a class="toggle">Toggle Edit Mode</a>
-                                $(".toggle").click(function () {
-                                   $("div").toggleClass("hidden unhidden");
-                                }*/}
-                                {/*also this: https://www.pair.com/support/kb/how-to-use-jquery-to-show-hide-a-form-on-click/*/}
-                            </div>
+                            
+                        </td>
+                        <div className="advanced_spark">
+                            {/*
+                    driver_mem = int(request.form['driver_mem'])
+                    max_result_mem = int(request.form['max_result_mem'])
+                            */}
                         </div>
-                    </div>
-                    <div className="col no-gutters d-flex justify-content-center">
-                        {/*right side*/}
-                        {/*advanced spark options*/}
-                        {/*appears when advanced options is toggled on*/}
-                        <div className="build-form right-side">
-                            <p>This is my form</p>
-                        </div>
-                    </div>
-                    <div className="build-btns">
+                        {/*https://www.pair.com/support/kb/how-to-use-jquery-to-show-hide-a-form-on-click/*/}
+                    </tr>
+                    <tr>
                         <Link to="/SelectNewDataset" className="back btn btn-secondary">
                             Back
                         </Link>
                         <Button className="start btn btn-primary" type="submit">Start Preprocessing</Button>
-                    </div>
+                    </tr>
                 </form>
             </div>
         );
