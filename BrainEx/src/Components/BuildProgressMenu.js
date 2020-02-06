1d2b3394ae6017fbd5c9b9607efba7a696932871
mags1 <magoodwin@wpi.edu>
2020-02-05 22:22:38 -0500

    refactored BuildProgressMenu.js and provided example of what would be displayed to users while build is in progress versus finished (click the header title to toggle page content)

--- BrainEx/src/Components/BuildProgressMenu.js
+++ BrainEx/src/Components/BuildProgressMenu.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import {Link} from "react-router-dom";
+import {Link as RouterLink} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../Stylesheets/BuildProgressMenu.css";
-import ProgressBar from 'react-bootstrap/ProgressBar'
+import {Link, ButtonGroup, Typography, LinearProgress } from '@material-ui/core';
+import $ from "jquery";
 
 // todo -- my concern: refreshing the page makes the form data disappear. user will lose progress if they refresh the window.
 //  how to get around this? looking into it but it is not a priority until we know what server/storage specs we have...
@@ -12,32 +13,87 @@ class BuildProgressMenu extends Component {
         super(props);
         this.state = {
             form_data: this.props.form_data,
-            progress: 60
         };
+    }
+
+    componentDidMount() {
+        $(".title").click(function(){
+            if ($(".in-progress").is(":visible")) {
+                $(".display").hide();
+                $(".hide").show();
+            } else {
+                $(".display").show();
+                $(".hide").hide();
+            }
+        });
     }
 
     render() {
         return (
             <div className="full-height"> {/* todo replace any height: 100% with "full-height" class on App.css */}
-                <div className="prog-content">
                     {/*todo make it so progress bar quickly finishes*/}
                     {/*todo make certain versions of the screen show depending on the value of "now" (is it done or not?)*/}
-                    <ProgressBar animated now={this.state.progress} label={`${this.state.progress}%`} />
-                    {/*below is for when build is finished*/}
-                    <h5 className="prog-item">Preprocessing stage is complete!</h5>
-                    <h5 className="prog-item">Sequences Processed: N/A </h5>
-                    <div className="prog-item">
-                        <Link to="/" className="btn btn-secondary">
-                            Restart with another dataset
-                        </Link>
-                        <Link to="/ExploreClusters" className="btn btn-secondary">
-                            Explore loaded data
-                        </Link>
-                        <Link to="/QueryFinder" className="btn btn-primary">
-                            Find Similar Sequences
-                        </Link>
+                    {/*display when build is in progress*/}
+                    <div className="prog-content in-progress display">
+                        <LinearProgress className="progress" />
+                        <Typography className="prog-item" variant="h6">Preprocessing is currently in progress</Typography>
+                        <ButtonGroup className="prog-item">
+                            <Link
+                                className="btn btn-secondary"
+                                variant="button"
+                                color="default"
+                                underline="none"
+                                component={RouterLink}
+                                to="/">
+                                Return to Home Page
+                            </Link>
+                            <Link
+                                className="btn btn-secondary"
+                                variant="button"
+                                color="default"
+                                underline="none"
+                                component={RouterLink}
+                                to="/BuildOptions">
+                                Cancel
+                            </Link>
+                        </ButtonGroup>
                     </div>
-                </div>
+                    {/*display below is for when build is finished*/}
+                    <div className="prog-content finished hide">
+                        <LinearProgress variant="determinate" value={100} className="progress"/>
+                        <Typography className="prog-item" variant="h6">Preprocessing stage is complete!</Typography>
+                        {/*todo by Sequences does she mean lines of data?*/}
+                        <Typography className="prog-item" variant="h6">Sequences Processed: N/A</Typography>
+                        <ButtonGroup className="prog-item">
+                            <Link
+                                className="btn btn-secondary"
+                                variant="button"
+                                color="default"
+                                underline="none"
+                                component={RouterLink}
+                                to="/">
+                                Restart with another dataset
+                            </Link>
+                            <Link
+                                className="btn btn-primary"
+                                variant="button"
+                                color="default"
+                                underline="none"
+                                component={RouterLink}
+                                to="/ExploreClusters">
+                                Explore loaded data
+                            </Link>
+                            <Link
+                                className="btn btn-primary"
+                                variant="button"
+                                color="default"
+                                underline="none"
+                                component={RouterLink}
+                                to="/QueryFinder">
+                                Find Similar Sequences
+                            </Link>
+                        </ButtonGroup>
+                    </div>
             </div>
         );
     }
