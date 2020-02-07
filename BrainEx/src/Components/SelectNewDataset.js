17c5b35e420a059a72ffc54031bae50285ec7ced
mags1 <magoodwin@wpi.edu>
2020-02-07 14:38:16 -0500

    setting up file functionality: creating FormData object to send to server, updating state when files are added, adding multiple files at once, clicking on a file in the list and displaying the filename (must use currentTarget not target).

--- BrainEx/src/Components/SelectNewDataset.js
+++ BrainEx/src/Components/SelectNewDataset.js
@@ -1,43 +1,74 @@
 import React, { Component } from 'react';
-import { rawdata_files } from "../dummy_data";
+import {preprocessed_files, rawdata_files} from "../dummy_data";
 import '../Stylesheets/Home.css';
 import { Button, Link, Typography, ButtonGroup } from '@material-ui/core';
 import { Link as RouterLink } from "react-router-dom";
+import FormData from "form-data";
+import $ from "jquery";
 
 class SelectNewDataset extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
+            currentFile: {},
             upload_files: null,
             all_files: rawdata_files
         };
         this.onChangeHandler = this.onChangeHandler.bind(this);
         this.onClickHandler = this.onClickHandler.bind(this);
+        this.showFilename = this.showFilename.bind(this);
     }
+
+    // pull files from database here. this function will be called after render()
+    componentDidMount() {
+        this.setState({
+            all_files: rawdata_files
+        });
+    }
+
+    // use this function to do any handling once a file in the list is selected
+    showFilename = (e) => {
+        $(".curr-file").show();
+        let currFile = e.currentTarget;
+        console.log(currFile.name);
+        this.setState({
+            currentFile: currFile
+        });
+    };
 
     // todo link used for file upload logic:
     //  https://programmingwithmosh.com/javascript/react-file-upload-proper-server-side-nodejs-easy/
     onChangeHandler = (e) => {
         // convert FileList to an array of files
-        const files = [...e.target.files];
-        let filenames = files.map((file) => {
-            return file.name;
-        });
-        // add the selected files to state so they can be accessed in onClickHandler
+        const new_files = [...e.target.files];
         this.setState({
-                upload_files: filenames
-            }, () => console.log(this.state.upload_files)); // print state for debugging
+                upload_files: new_files
+            }, () => {
+            console.log("files added to state successfully:");
+            console.log(this.state.upload_files) // cannot print text and object in the same console.log
+        }); // print state for debugging
     };
 
     //handles the "Add" button functionality to add selected file to the server
     // todo eventually have this add the file to the "database" - mg
     onClickHandler = (e) => {
+        e.preventDefault(); // prevents page refresh
+        let file_form = new FormData();
         let new_files = this.state.upload_files;
+        new_files.map((file) => {
+            file_form.append("file", file); // add upload_files to FormData object
+        });
         let all_files = this.state.all_files;
+        console.log(...file_form);
+        // todo instead of below, send FormData to server and make state update somehow
         this.setState({
-            all_files: all_files.concat(new_files)
-        }, () => console.log(this.state.all_files));
+            all_files: all_files.concat(new_files),
+            upload_files: null
+        }, () => {
+            console.log("files \"uploaded\" successfully");
+            console.log(this.state.all_files);
+        });
     };
 
     render() {
@@ -50,23 +81,24 @@ class SelectNewDataset extends Component {
                                 <Typography className="directions" variant="h4">Select a dataset to preview here</Typography>
                                 <ButtonGroup className="file-list" orientation="vertical" color="primary">
                                     { this.state.all_files.map((file, index) => (
-                                        <Button className="btn-file" variant="contained" key={index}>{file}</Button>
+                                       <Button name={file.name} className="btn-file" variant="contained" key={index} onClick={this.showFilename}>{file.name}</Button>
                                     ))}
                                 </ButtonGroup>
                                 {/*adding a new file*/}
-                                <div className="form-group files home-content">
+                                <form className="form-group files home-content">
                                     <Typography className="directions" variant="h5">Load another dataset</Typography>
                                     <div className="new-file d-flex justify-content-center align-items-center">
                                         <input type="file" name="file" className="form-control-file" accept=".csv" onChange={this.onChangeHandler} multiple/>
-                                        <Button className="btn-primary" variant="contained" color="primary" onClick={this.onClickHandler}>Add</Button>
+                                        <Button type="submit" className="btn-primary" variant="contained" color="primary" onClick={this.onClickHandler}>Add</Button>
                                     </div>
-                                </div>
+                                </form>
                             </div>
                         </div>
                     </div>
                     <div className="col no-gutters">
                         <div className="right build">
                             <div className="home-content">
+                                <p className="curr-file">{this.state.currentFile.name}</p>
                                 <Link
                                     className="build-btn right-btn btn btn-primary"
                                     variant="button"
--- BrainEx/src/dummy_data.js
+++ BrainEx/src/dummy_data.js
@@ -1,6 +1,24 @@
-let preprocessed_files = ["SART1", "SART2", "SART3", "SART4", "SART5", "SART6", "SART7", "SART8", "SART9", "SART10"];
+let preprocessed_files = [{"name":"NullamSitAmet.xls","lastModified":1719938833282,"lastModifiedDate":"9/29/2019","webkitRelativePath": "","size":614626,"type":"application/excel"},
+{"name":"Cubilia.xls","lastModified":6274044857369,"lastModifiedDate":"4/2/2019","webkitRelativePath": "","size":997642,"type":"application/excel"},
+{"name":"MiInPorttitor.xls","lastModified":2459266468630,"lastModifiedDate":"8/14/2019","webkitRelativePath": "","size":854839,"type":"application/x-excel"},
+{"name":"SitAmet.ppt","lastModified":5882685687827,"lastModifiedDate":"10/6/2019","webkitRelativePath": "","size":577832,"type":"application/x-mspowerpoint"},
+{"name":"InFaucibus.xls","lastModified":8518083743417,"lastModifiedDate":"1/16/2020","webkitRelativePath": "","size":188241,"type":"application/x-msexcel"},
+{"name":"OdioCurabitur.doc","lastModified":6154986054543,"lastModifiedDate":"7/11/2019","webkitRelativePath": "","size":504872,"type":"application/msword"},
+{"name":"IaculisCongue.ppt","lastModified":6861802684667,"lastModifiedDate":"5/15/2019","webkitRelativePath": "","size":208705,"type":"application/vnd.ms-powerpoint"},
+{"name":"VelitEuEst.xls","lastModified":7150927409271,"lastModifiedDate":"2/25/2019","webkitRelativePath": "","size":156423,"type":"application/excel"},
+{"name":"NullaElitAc.xls","lastModified":3434965732380,"lastModifiedDate":"4/9/2019","webkitRelativePath": "","size":383619,"type":"application/x-msexcel"},
+{"name":"SagittisDui.ppt","lastModified":4886521473863,"lastModifiedDate":"3/7/2019","webkitRelativePath": "","size":523041,"type":"application/mspowerpoint"}];
 
-let rawdata_files = ["Dataset1", "Dataset2", "Dataset3", "Dataset4", "Dataset5", "Dataset6", "Dataset7", "Dataset8", "Dataset9", "Dataset10"];
+let rawdata_files = [{"name":"Hac.xls","lastModified":1768564955364,"lastModifiedDate":"3/25/2019","webkitRelativePath":null,"size":779369,"type":"application/x-excel"},
+{"name":"AcNibh.pdf","lastModified":4384460909598,"lastModifiedDate":"12/9/2019","webkitRelativePath":null,"size":884471,"type":"application/pdf"},
+{"name":"PulvinarLobortisEst.xls","lastModified":8503021164249,"lastModifiedDate":"8/14/2019","webkitRelativePath":null,"size":115066,"type":"application/x-excel"},
+{"name":"In.ppt","lastModified":9638748803313,"lastModifiedDate":"10/4/2019","webkitRelativePath":null,"size":586187,"type":"application/mspowerpoint"},
+{"name":"UllamcorperPurus.ppt","lastModified":5860961976564,"lastModifiedDate":"7/7/2019","webkitRelativePath":null,"size":266449,"type":"application/vnd.ms-powerpoint"},
+{"name":"DonecDiamNeque.ppt","lastModified":2318021333325,"lastModifiedDate":"7/22/2019","webkitRelativePath":null,"size":418835,"type":"application/x-mspowerpoint"},
+{"name":"ElementumPellentesque.xls","lastModified":7025445068997,"lastModifiedDate":"1/26/2020","webkitRelativePath":null,"size":649757,"type":"application/x-msexcel"},
+{"name":"NasceturRidiculus.xls","lastModified":6619432384404,"lastModifiedDate":"4/7/2019","webkitRelativePath":null,"size":402203,"type":"application/x-excel"},
+{"name":"ConsectetuerAdipiscingElit.ppt","lastModified":7752637037926,"lastModifiedDate":"4/22/2019","webkitRelativePath":null,"size":996998,"type":"application/x-mspowerpoint"},
+{"name":"Blandit.xls","lastModified":7178327907008,"lastModifiedDate":"5/4/2019","webkitRelativePath":null,"size":825632,"type":"application/excel"}];
 
 export {
     preprocessed_files,
