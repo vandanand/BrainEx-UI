30eadc8daf04e2502099fd71fac4ccd96bbb7c09
mags1 <magoodwin@wpi.edu>
2020-02-07 18:38:13 -0500

    added mui app header

--- BrainEx/src/Components/BrainExHeader.js
+++ BrainEx/src/Components/BrainExHeader.js
@@ -1,17 +1,25 @@
 import React, {Component} from 'react';
 import logo from "../brain.png";
+import { AppBar, Toolbar, Typography, Button } from "@material-ui/core";
 
 class BrainExHeader extends Component {
 
     render() {
         return(
-            <div className="header">
+            <AppBar position="static">
+                <Toolbar className="info">
+                    <div className="logo"><img src={logo}  alt="logo"/></div>
+                    <Typography className="title" variant="h1">BrainEx</Typography>
+                    <Typography className="version" variant="h3">Version 1.0.0</Typography>
+                </Toolbar>
+            </AppBar>
+            /*<div className="header">
                 <div className="info">
                     <div className="logo"><img src={logo}  alt="logo"/></div>
                     <h1 className="title">BrainEx</h1>
                     <h3 className="version">Version 1.0.0</h3>
                 </div>
-            </div>
+            </div>*/
         );
     }
 }
