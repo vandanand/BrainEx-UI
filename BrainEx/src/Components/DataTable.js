d823d781b6315ec4c89f7cb9c8bbf2d432f917ff
mags1 <magoodwin@wpi.edu>
2020-02-17 11:33:38 -0500

    committing changes so i can switch branches

--- BrainEx/src/Components/DataTable.js
+++ BrainEx/src/Components/DataTable.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -10,6 +10,10 @@ import Checkbox from '@material-ui/core/Checkbox';
 import { query_results_dd } from "../data/query_results_dd";
 import Rainbow from 'rainbowvis.js/rainbowvis.js';
 import {top_color, bottom_color} from '../data/default_values';
+
+const useStyles = makeStyles(theme => ({
+    // styles go here
+}));
 
 // creates a row of data
 function createData(id, toggle, color, subjectID, eventName, channelNum, startTime, endTime) {
@@ -38,28 +42,63 @@ function generateColors(numColors, top_color, bottom_color) {
     return r;*/
 }
 
-// function to create the data table content using an external source (in this case, a constant from another file)
-function createTable(data) {
-    const table = [];
-    let colors = generateColors(data.length, top_color, bottom_color);
-    console.log(colors);
-    data.map( (row, index) => {
-        // todo add checkbox functionality here
-        let length = table.push(createData(row.id, <Checkbox/>, colors[index], row.subjectID, row.eventName, row.channelNum, row.startTime, row.endTime));
-        // console.log("length: " + length);
-    });
-    console.log(table);
-    return table;
-}
-
-const useStyles = makeStyles(theme => ({
-    // styles go here
-}));
+// initialize list of checkbox values to be all true, same number of items as rows in data
+const initializeCheckboxValues = (data) => {
+    console.log("calling initialize checkboxes");
+    let numCheckboxes = data.length;
+    // create list of checkbox values (initialized to true)
+    let checkbox_values = []; // value to be stored in showSequence (the state values are true/false)
+    for (let i=0; i<numCheckboxes; i++) {
+        checkbox_values.push({id: i, isChecked: true});
+    }
+    console.log(checkbox_values);
+    return checkbox_values;
+};
 
 export default function DataTable() {
+
+    useEffect(() => {
+        // todo @Kyra in useEffects, pull in the data from the API for initial pull, updating requires some extra thought I have to look into
+    });
+
     const classes = useStyles();
     // setData should not be used unless we expect some sort of update while the user is looking at the data
+    const [showSequence, setShowSequence] = useState(initializeCheckboxValues(query_results_dd)); // list of ids and isChecked t/f json's
+    const [checkBoxes, setCheckboxVal] = useState(initializeCheckboxes(query_results_dd));
     const [data, setData] = useState(createTable(query_results_dd));
+
+    function initializeCheckboxes(data) {
+        let checkboxes = [];
+        let numCheckboxes = data.length;
+        const handleCheckboxChange = (e, i) => {
+            console.log(e);
+            console.log(e.target);
+            console.log(e.target.checked);
+            console.log(e.currentTarget);
+            console.log(e.currentTarget.checked);
+            let newValues = showSequence;
+            newValues[i] = e.target.checked;
+            setShowSequence(newValues);
+        };
+        for (let i=0; i<numCheckboxes; i++) {
+
+        }
+    }
+
+    // function to create the data table content using an external source (in this case, a constant from another file)
+    function createTable(data) {
+        const table = [];
+        let colors = generateColors(data.length, top_color, bottom_color);
+        data.map( (row, index) => {
+            // todo add checkbox functionality here
+            // todo should id of checkbox be index or row.id?
+            // todo for the state value of this checkbox have an array of true/false and reference it by index when updating/displaying
+            let length = table.push(createData(row.id, checkboxes[index], colors[index], row.subjectID, row.eventName, row.channelNum, row.startTime, row.endTime));
+            // console.log("length: " + length);
+        });
+        console.log(table);
+        return table;
+    }
 
     return (
         <React.Fragment>
