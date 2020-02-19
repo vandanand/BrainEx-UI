69fae4fcdf578aab81eeb0ad79aaee8c8bf218bd
mags1 <magoodwin@wpi.edu>
2020-02-19 13:39:26 -0500

    CurrSeqSelection.js - made file a state variable

--- BrainEx/src/Components/CurrSeqSelection.js
+++ BrainEx/src/Components/CurrSeqSelection.js
@@ -1,12 +1,10 @@
-import React from 'react';
+import React , {useState} from 'react';
 import {makeStyles} from "@material-ui/core/styles";
 import Title from "./Title";
 import Button from '@material-ui/core/Button';
 import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 import CurSeqLineChart from "./CurSeqLineChart";
 import axios from 'axios';
-
-var file = null;
 
 function preventDefault(event) {
     event.preventDefault();
@@ -21,27 +19,32 @@ const useStyles = makeStyles({
     }
 });
 
-function onChangeHandler(e){
-  file = e.target.files[0]
-}
-
-function onClickHandler(e) {
-  e.preventDefault(); // prevents page refresh on submit
-  /* create form data object and append files to be uploaded onto it*/
-  let file_form = new FormData();
-  file_form.append("sequence_file", file);
-  // Hook up to Kyra's server
-  axios.post('http://localhost:5000/uploadSequence', file_form)
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-}
-
 export default function CurrSeqSelection() {
     const classes = useStyles();
+
+    const [file, setFile] = useState(null);
+
+    function onChangeHandler(e) {
+        let newFile = e.currentTarget.files[0];
+        setFile(newFile);
+        console.log(file);
+    }
+
+    function onClickHandler(e) {
+        e.preventDefault(); // prevents page refresh on submit
+        /* create form data object and append files to be uploaded onto it*/
+        let file_form = new FormData();
+        file_form.append("sequence_file", file);
+        // Hook up to Kyra's server
+        axios.post('http://localhost:5000/uploadSequence', file_form)
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
     return (
         <React.Fragment>
             <div className={classes.root}>
@@ -51,7 +54,6 @@ export default function CurrSeqSelection() {
                     accept="text/csv/*"
                     className={classes.input}
                     id="outlined-button-file"
-                    multiple
                     type="file"
                     onChange={onChangeHandler}
                 />
