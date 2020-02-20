91dc4ed81013ea77ddcf0408c4e74848a07fd93e
mags1 <magoodwin@wpi.edu>
2020-02-19 19:00:31 -0500

    colors now stores the # along w the hex value

--- BrainEx/src/Components/Preprocessing/BuildProgressMenu.js
+++ BrainEx/src/Components/Preprocessing/BuildProgressMenu.js
@@ -17,18 +17,35 @@ class BuildProgressMenu extends Component {
             loi_max: this.props.location.state.loi_max,
             open: false,
             message: null,
-            mode: null
+            mode: null,
+            isPreprocessing: true
         };
         this.openModal = this.openModal.bind(this);
         this.closeModal = this.closeModal.bind(this);
         this.cancelPreprocessing = this.cancelPreprocessing.bind(this);
     }
 
-    componentDidMount() {
-        // for debugging
-        console.log(this.state.loi_min);
-        console.log(this.state.loi_max);
+    // todo @Kyra: https://medium.com/@pkellner/fetch-data-in-react-with-spinner-only-when-necessary-1ef2e451f541
+    componentWillMount() {
+        // todo axios get call here to listen for build complete?
+        /* todo something like this:
+        *  axios.get("https://api.github.com/users/pkellner/repos")
+            .then((result) => {
+                this.setState({
+                    appData: result.data,
+                    isLoading: false
+                });
+            })
+            .catch(error => {
+                if (error.response) {
+                    console.log(error.responderEnd);
+                }
+            });
+        * */
+    }
 
+    componentDidMount() {
+        // todo remove this listener when progress bar backend functionality is complete
         $(".info").click(function(){
             if ($(".in-progress").is(":visible")) {
                 $(".display-this").hide();
@@ -40,6 +57,7 @@ class BuildProgressMenu extends Component {
         });
     }
 
+    // open model by setting all modal state values and displaying appropriate message
     openModal = (mode) => {
         let message = "";
         if (mode === "cancel") {
@@ -105,79 +123,81 @@ class BuildProgressMenu extends Component {
                         </Button>
                     </DialogActions>
                 </Dialog>
-
-                {/*display when build is in progress*/}
-                <div className="prog-content in-progress display-this">
-                    <LinearProgress className="progress" />
-                    <Typography className="prog-item" variant="h4">Preprocessing is currently in progress</Typography>
-                    <ButtonGroup className="prog-item">
-                        {/*<Link
-                            className="btn btn-secondary"
-                            variant="button"
-                            color="default"
-                            underline="none"
-                            component={RouterLink}
-                            to={root}>
-                            Return to Home Page
-                        </Link>*/}
-                        <Button className="btn btn-secondary" color="default" onClick={this.openModal("home")}>
-                            Cancel and return to home
-                        </Button>
-                        <Button className="btn btn-secondary" color="default" onClick={this.openModal("cancel")}>
-                            Cancel
-                        </Button>
-                        {/*<Link
-                            className="btn btn-secondary"
-                            variant="button"
-                            color="default"
-                            underline="none"
-                            component={RouterLink}
-                            to={build_options}>
-                            Cancel
-                        </Link>*/}
-                    </ButtonGroup>
-                </div>
-                {/*display below is for when build is finished*/}
-                <div className="prog-content finished hide-this">
-                    <LinearProgress variant="determinate" value={100} className="progress"/>
-                    <Typography className="prog-item" variant="h4">Preprocessing stage is complete!</Typography>
-                    {/*todo by Sequences does she mean lines of data?*/}
-                    {/*<Typography className="prog-item" variant="h4">Sequences Processed: N/A</Typography>*/}
-                    <ButtonGroup className="prog-item">
-                        <Link
-                            className="btn btn-secondary"
-                            variant="button"
-                            color="default"
-                            underline="none"
-                            component={RouterLink}
-                            to={root}>
-                            Restart with another dataset
-                        </Link>
-                        <Link
-                            className="btn btn-primary"
-                            variant="button"
-                            color="default"
-                            underline="none"
-                            component={RouterLink}
-                            to={data_exp}>
-                            Explore loaded data
-                        </Link>
-                        <Link
-                            className="btn btn-primary"
-                            variant="button"
-                            color="default"
-                            underline="none"
-                            component={RouterLink}
-                            to={{
-                                pathname: `${query_page}`,
-                                state: {
-                                    loi_max: this.state.loi_max,
-                                    loi_min: this.state.loi_min
-                                }}}>
-                            Find Similar Sequences
-                        </Link>
-                    </ButtonGroup>
-                </div>
+                {(this.state.isPreprocessing) ? (
+                    /*display when build is in progress*/
+                    <div className="prog-content in-progress display-this">
+                        <LinearProgress className="progress" />
+                        <Typography className="prog-item" variant="h4">Preprocessing is currently in progress</Typography>
+                        <ButtonGroup className="prog-item">
+                            <Link
+                                className="btn btn-secondary"
+                                variant="button"
+                                color="default"
+                                underline="none"
+                                component={RouterLink}
+                                to={root}>
+                                Return to Home Page
+                            </Link>
+                            <Button className="btn btn-secondary" color="default" onClick={this.openModal("home")}>
+                                Cancel and return to home
+                            </Button>
+                            <Button className="btn btn-secondary" color="default" onClick={this.openModal("cancel")}>
+                                Cancel
+                            </Button>
+                            <Link
+                                className="btn btn-secondary"
+                                variant="button"
+                                color="default"
+                                underline="none"
+                                component={RouterLink}
+                                to={build_options}>
+                                Cancel
+                            </Link>
+                        </ButtonGroup>
+                    </div>
+                ) : (
+                    /*display below is for when build is finished*/
+                    <div className="prog-content finished hide-this">
+                        <LinearProgress variant="determinate" value={100} className="progress"/>
+                        <Typography className="prog-item" variant="h4">Preprocessing stage is complete!</Typography>
+                        todo by Sequences does she mean lines of data?
+                        <Typography className="prog-item" variant="h4">Sequences Processed: N/A</Typography>
+                        <ButtonGroup className="prog-item">
+                            <Link
+                                className="btn btn-secondary"
+                                variant="button"
+                                color="default"
+                                underline="none"
+                                component={RouterLink}
+                                to={root}>
+                                Restart with another dataset
+                            </Link>
+                            <Link
+                                className="btn btn-primary"
+                                variant="button"
+                                color="default"
+                                underline="none"
+                                component={RouterLink}
+                                to={data_exp}>
+                                Explore loaded data
+                            </Link>
+                            <Link
+                                className="btn btn-primary"
+                                variant="button"
+                                color="default"
+                                underline="none"
+                                component={RouterLink}
+                                to={{
+                                    pathname: `${query_page}`,
+                                    state: {
+                                        loi_max: this.state.loi_max,
+                                        loi_min: this.state.loi_min
+                                    }}}>
+                                Find Similar Sequences
+                            </Link>
+                        </ButtonGroup>
+                    </div>
+                )}
             </div>
         );
     }
