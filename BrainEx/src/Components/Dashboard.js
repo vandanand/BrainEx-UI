1dfb7477a41ec54bd4c0d701a18413c94226e396
kyrab <38116068+krbrez@users.noreply.github.com>
2020-02-24 01:05:12 -0500

    Get upload sequence working again

--- BrainEx/src/Components/Dashboard.js
+++ BrainEx/src/Components/Dashboard.js
@@ -72,6 +72,7 @@ export default function Dashboard(props) {
     function receiveData(tableData) {
         console.log("calling receiveData");
         setData(tableData);
+        console.log(receivedData);
     }
 
     return (
